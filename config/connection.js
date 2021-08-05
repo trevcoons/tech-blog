b9d7700e571c74366fe09a1132b4b338993b86c9
twizard <trevcoons@gmail.com>
2021-08-05 15:37:05 -0700

    refactor

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,6 @@
-// Dependencies
-// Sequelize constructor
+
 const Sequelize = require('sequelize');
-// dotenv for local environmental variables for user name and password
+
 require('dotenv').config();
 
 let sequelize;
@@ -12,7 +11,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    port: 3001
   });
 }
 
